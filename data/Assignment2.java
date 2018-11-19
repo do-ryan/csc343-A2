d7904a64c9e02e73ec1a100cf0fec045dd2c54a8
Ryan Do <ryan.do@mail.utoronto.ca>
2018-11-19 17:55:45 -0500

    added Cullen's part 2

--- data/Assignment2.java
+++ data/Assignment2.java
@@ -4,7 +4,7 @@ import java.util.List;
 // If you are looking for Java data structures, these are highly useful.
 // Remember that an important part of your mark is for doing as much in SQL (not Java) as you can.
 // Solutions that use only or mostly Java will not receive a high mark.
-//import java.util.ArrayList;
+import java.util.ArrayList;
 //import java.util.Map;
 //import java.util.HashMap;
 //import java.util.Set;
@@ -18,27 +18,83 @@ public class Assignment2 extends JDBCSubmission {
 
     @Override
     public boolean connectDB(String url, String username, String password) {
-        // Implement this method!
-	
-        return false;
+        //set the search path
+
+        //get the connection based on the credentials
+        try {
+            this.connection = DriverManager.getConnection(url, username, password);
+        }
+        catch ( SQLException err ) {
+            System.out.println("Cannot connect to the database");
+            return false;
+        }
+        return true;
     }
 
     @Override
     public boolean disconnectDB() {
-        // Implement this method!
-        return false;
+        try {
+            this.connection.close();
+        }
+        catch ( SQLException err ) {
+            System.out.println("Could not close the database");
+            return false;
+        }
+        return true;
     }
 
     @Override
     public ElectionCabinetResult electionSequence(String countryName) {
-        // Implement this method!
-        return null;
+
+        try {
+            Statement statement = connection.createStatement();
+            String query = "SELECT election.id, cabinet.id " +
+                    "FROM election, cabinet " +
+                    "WHERE election.id = cabinet.election_id AND election.country_id = cabinet.country_id = " +
+                    countryName + " " +
+                    "ORDER BY election.e_date DESC";
+
+            ResultSet set = statement.executeQuery(query);
+
+            //loop through the result set and return a 2d array which is an ElectionCabinetResult
+            //create to arrays then feed into ElectionCabinetResult
+            ArrayList<Integer> cabinet = new ArrayList<>();
+            ArrayList<Integer> election = new ArrayList<>();
+
+            while (set.next()) {
+                election.add(set.getInt(1));
+                cabinet.add(set.getInt(2));
+            }
+            return new ElectionCabinetResult(election, cabinet);
+        } catch (SQLException e){
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public List<Integer> findSimilarPoliticians(Integer politicianName, Float threshold) {
-        // Implement this method!
-        return null;
+        ArrayList<Integer> output = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String query = "SELECT p2.id, p1.description || ' ' || p1.comments as compare1, p2.description || ' ' ||" +
+                    " p2.comments as compare2 " +
+                    "FROM politician_president p1, politician president p2 " +
+                    "WHERE p1.id = " + politicianName + " AND p1.id < p2.id";
+
+            ResultSet set = statement.executeQuery(query);
+
+            //if the description and comments pass the threshold, then retain the id of the second politician
+            while (set.next()) {
+                if (similarity(set.getString(2), set.getString(3)) > threshold){
+                    output.add(set.getInt(1));
+                }
+            }
+            return output;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public static void main(String[] args) {
